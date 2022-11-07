10b7c207857f69e637cde25f02741bb6d33b06c3
Salma Wangara <sallywangara20@gmail.com>
2022-11-07 12:37:32 +0300

    Changed forms.html

---
+++ file.js
@@ -0,0 +1,31 @@
+function Page() {
+  return (
+    <div>
+      <header>
+        <nav className="header">
+          <img
+            src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+            className="logo"
+          />
+          <ul className="list">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+      <h1>Reasons why I'm excited to be learning React</h1>
+      <ol>
+        <li>It is fun</li>
+        <li>It is educational</li>
+        <li>It is attributive</li>
+        <li>It is exciting</li>
+      </ol>
+      <footer className="footer">
+        <small>© 2022 Wangara development. All rights reserved.</small>
+      </footer>
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById('hero'));
